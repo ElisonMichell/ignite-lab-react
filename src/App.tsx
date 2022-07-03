fc9bddef48a88e16f2303bc6ebe31dae1b442a81
Elison Michell <dev.elisonmichell@gmail.com>
2022-07-03 18:30:56 -0300

    :bug: fix: Fix missing icon import

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,11 @@ import { ApolloProvider } from "@apollo/client";
 import { BrowserRouter } from "react-router-dom";
 import { client } from "./lib/apollo";
 import { Router } from "./Router";
+import rocketseatIcon from "./assets/rocketseat-icon.png"
 
 export function App() {
+
+  document.querySelector("link")!.href = rocketseatIcon;
 
   return (
     <ApolloProvider client={client}>
